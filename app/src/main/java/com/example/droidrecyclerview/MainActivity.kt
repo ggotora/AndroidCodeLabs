6fcedd4338fb5350d98a22f5ecfc61e1ca2a3a6c
Gilbert Gotora <gilbertgotora2@gmail.com>
2022-01-10 12:14:02 +0200

    Add Datasource.kt and Affirmation data class

--- app/src/main/java/com/example/droidrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/droidrecyclerview/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.droidrecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.droidrecyclerview.data.Datasource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.tvDisplay)
+
+        textView.setOnClickListener {
+            textView.text = Datasource().loadAffirmations().size.toString()
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/droidrecyclerview/model/Affirmation.kt
@@ -0,0 +1,3 @@
+package com.example.droidrecyclerview.model
+
+data class Affirmation (val stringResourceId: Int)
